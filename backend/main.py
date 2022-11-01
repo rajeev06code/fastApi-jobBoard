00f2f895205751d16a7906bc1b6e01b175fb0bf6
rajeev06code <rajeev_sharma15@outlook.com>
2022-11-01 18:46:57 +0530

    initial commit

---
+++ backend/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from core.config import settings
+from apis.base import api_router
+from apis.general_pages.route_homepage import general_pages_router
+from db.session import engine
+from db.base import Base
+
+def include_router(app):
+	app.include_router(api_router)
+
+def create_tables():           #new
+	Base.metadata.create_all(bind=engine)
+
+def start_application():
+ app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
+ include_router(app)
+ create_tables()
+ return app 
+
+app = start_application()
---
+++ backend/schemas/users.py
@@ -0,0 +1,17 @@
+from typing import Optional
+from pydantic import BaseModel,EmailStr
+
+
+#properties required during user creation
+class UserCreate(BaseModel):
+    username: str
+    email : EmailStr
+    password : str
+    
+class ShowUser(BaseModel):   #new
+    username : str 
+    email : EmailStr
+    is_active : bool
+
+    class Config():
+        orm_mode = True
